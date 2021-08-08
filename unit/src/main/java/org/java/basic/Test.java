61e98c9442ca377f95b7add582567e938bccb96f
ouxu <gyouxu@gmail.com>
2021-08-08 12:15:44 +0200

    Thread

--- unit/src/main/java/org/java/basic/Test.java
+++ unit/src/main/java/org/java/basic/Test.java
@@ -1,11 +1,41 @@
 package org.java.basic;
 
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Test {
     public static final int capacity = 1 << 30;
 
     public static void main(String[] args) {
         System.out.println(capacity);
         System.out.println(tableSizeFor(31));
+//        test();
+        List<GarbageCollectorMXBean> garbageCollectorMXBeans = ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean mben :
+                garbageCollectorMXBeans) {
+            System.out.println("--->"+mben.getCollectionCount());
+        }
+//        testString();
+    }
+
+    private static void test() {
+        long begin = System.currentTimeMillis();
+        List<String> collect = IntStream.rangeClosed(1, 10000000).mapToObj(i -> String.valueOf(i).intern()).collect(Collectors.toList());
+        System.out.println(System.currentTimeMillis() - begin);
+
+    }
+
+    private static void testString() {
+        Integer a = 127;
+        Integer b = 127;
+        System.out.println(a == b);
+        String aa = "1";
+        String bb = new String("1");
+        System.out.println(aa == bb);
+        System.out.println(aa.equals(bb));
     }
 
     static final int tableSizeFor(int cap) {
